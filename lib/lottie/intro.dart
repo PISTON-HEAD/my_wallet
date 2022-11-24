12a77553d6120466fb1a7bfc79faabfece166828
gokul <gokulkrishna1282@gmail.com>
2022-11-25 01:36:39 +0530

    Updation

--- lib/lottie/intro.dart
+++ lib/lottie/intro.dart
@@ -14,7 +14,7 @@ class _introsState extends State<intros> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(23,23,49,1),
+      backgroundColor: const Color.fromRGBO(23,23,49,1.0),
       body: Column(
         mainAxisAlignment:MainAxisAlignment.center,
         children: [
